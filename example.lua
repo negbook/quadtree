52e80235669d02865413814d40fe9c7dc029ddd3
negbook <negbook@gmail.com>
2022-04-04 01:53:40 +0800

    removed object because it can not stacked

--- example.lua
+++ example.lua
@@ -10,16 +10,7 @@ for i=1,10000 do
     local z = math.random(0,8000)
     tree:insert_point(vector3(x,y,z))
 end
-for i=1,10000 do 
-    local x = math.random(0,8000)
-    local y = math.random(0,8000)
-    local z = math.random(0,8000)
-    tree:insert_object("circle?",{
-        center = vector3(x,y,z),
-        size = vector3(1.0,1.0,1.0),
-        whatisthat = "circle"..(i)
-    })
-end
+
 
 print(tree)
 print(tree.topleft)
@@ -41,11 +32,3 @@ print("point by rectangle",#tree:query_points_by_rectangle({
 print("point by point",#tree:query_points_by_point(vector3(1.0,1.0,30.0),1000))
 
 print("point by circle",#tree:query_points_by_point(vector3(1.0,1.0,30.0),100000))
-
-print("object by point",#tree:query_objects_by_point("circle?",vector3(1.0,1.0,30.0),1000))
-
-print("object by rectangle",#tree:query_objects_by_rectangle("circle?",{
-    center = vector3(222.0,1.0,30.0),
-    size = vector3(40000.0,40000.0,40000.0)
-}))
-
